32378ea8157456c11191161211a56de24c0a5d4c
Saidu45 <Saiduth200@gmail.com>
2025-09-07 17:20:55 +0000

    Added The Register Functionality

--- src/app/register/register.ts
+++ src/app/register/register.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // Required for template-driven forms
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, RouterModule],
   templateUrl: './register.html',
-  styleUrl: './register.css'
+  styleUrls: ['./register.css']
 })
-export class Register {
+export class Register{
+  newUser = {
+    fullName: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  };
 
+  onSubmit(form: any) {
+    if (form.valid) {
+      if (this.newUser.password !== this.newUser.confirmPassword) {
+        alert('Passwords do not match!');
+        return;
+      }
+
+      console.log('Registration Submitted!', this.newUser);
+      alert(`Welcome aboard, ${this.newUser.fullName}! Your account has been created.`);
+      form.reset();
+    }
+  }
 }
